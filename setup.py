0763b9163da1e15b62497b37ebb9babebaef346e
David Hickman <davidhickman13@gmail.com>
2017-09-19 14:45:02 -0500

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='ahps_alerts',
-    version='0.1.4',
+    version='0.1.5',
     description="Retrieve NWS AHPS alerts",
     long_description=readme + '\n\n' + history,
     author="David Hickman",
